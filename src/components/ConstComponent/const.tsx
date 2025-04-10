466f168fe032921307839053e2dc0e608953c0c7
sofiya <enikeevasofiya@gmail.com>
2025-04-10 17:21:06 +0500

    feat(src): add kitchen block with adaptive

--- src/components/ConstComponent/const.tsx
+++ src/components/ConstComponent/const.tsx
@@ -7,15 +7,15 @@ import { Footer, Header, Navigation, Sidebar } from '../app';
 
 export const Const = () => (
     <Grid
-        templateRows={{ base: 'auto 1fr auto', lg: 'auto 1fr' }}
-        templateColumns={{ base: '1fr', lg: '256px 1fr 240px' }}
+        templateRows={{ base: 'auto 1fr auto', xl: 'auto 1fr' }}
+        templateColumns={{ base: '1fr', xl: '256px 1fr 240px' }}
         templateAreas={{
             base: `
                 "header"
                 "main"
                 "footer"
             `,
-            lg: `
+            xl: `
                 "header header header"
                 "nav main sidebar"
                 "footer footer footer"
@@ -29,7 +29,7 @@ export const Const = () => (
             <Header />
         </GridItem>
 
-        <GridItem area='nav' display={{ base: 'none', lg: 'grid' }} mt='84px'>
+        <GridItem area='nav' display={{ base: 'none', xl: 'grid' }} mt='84px'>
             <Navigation navigation={NAVIGATION} />
         </GridItem>
 
@@ -37,11 +37,11 @@ export const Const = () => (
             <Outlet />
         </GridItem>
 
-        <GridItem area='aside' display={{ base: 'none', lg: 'grid' }} mt='84px'>
+        <GridItem area='aside' display={{ base: 'none', xl: 'grid' }} mt='84px'>
             <Sidebar />
         </GridItem>
 
-        <GridItem area='footer' display={{ base: 'block', lg: 'none' }}>
+        <GridItem area='footer' display={{ base: 'block', xl: 'none' }}>
             <Footer />
         </GridItem>
     </Grid>
--- src/pages/veganPage.tsx
+++ src/pages/veganPage.tsx
@@ -1,1 +1,9 @@
-export const VeganPage = () => <div>vegan page</div>;
+import { Kitchen } from '~/components/KitchenComponent/kitchen';
+import { BAKE } from '~/constants/kitchen';
+
+export const VeganPage = () => (
+    <div>
+        vegan page
+        <Kitchen kitchen={BAKE} />
+    </div>
+);
