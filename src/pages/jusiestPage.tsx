466cabfbcfb5584c512990f60fd04aac8dd13f23
sofiya <enikeevasofiya@gmail.com>
2025-04-12 16:02:21 +0500

    feat(src): done with page Juiciest

--- src/pages/jusiestPage.tsx
+++ src/pages/jusiestPage.tsx
@@ -1,9 +1,24 @@
+import { Flex } from '@chakra-ui/react';
+
 import { Kitchen } from '~/components/KitchenComponent/kitchen';
+import { OnJuisiestPage } from '~/components/OnJuiciestPageComponent/onJuiciestPage';
+import { Search } from '~/components/SearchComponent/search';
+import { JUISIESTPAGE } from '~/constants/juisiest';
 import { VEGAN } from '~/constants/kitchen';
+
+import styles from './main.module.scss';
 
 export const JuiciestPage = () => (
     <div>
-        juiciest page
-        <Kitchen kitchen={VEGAN} />
+        <Flex>
+            <Flex className={styles.main}>
+                <Flex align='center' flexDirection='column'>
+                    <div className={styles.title}>Самое сочное</div>
+                    <Search />
+                    <OnJuisiestPage juisiest={JUISIESTPAGE} />
+                </Flex>
+                <Kitchen kitchen={VEGAN} />
+            </Flex>
+        </Flex>
     </div>
 );
