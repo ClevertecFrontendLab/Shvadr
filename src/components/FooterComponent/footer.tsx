aef27d810a97bf6fbaa21f10114815df196959c7
sofiya <enikeevasofiya@gmail.com>
2025-04-12 15:21:49 +0500

    fix(src): fix footer styles

--- src/components/FooterComponent/footer.tsx
+++ src/components/FooterComponent/footer.tsx
@@ -7,61 +7,60 @@ import { HomeIcon } from '~/components/Icons/HomeIcon';
 
 function Footer() {
     return (
-        <footer data-test-id='footer'>
-            <Flex
-                justify='space-around'
-                align='center'
-                className={styles.container}
-                display={{ base: 'flex', xl: 'none' }}
-            >
-                <Box>
-                    <Flex justify='center' align='center' h='40px' className={styles.home}>
-                        <HStack>
-                            <HomeIcon color='#ffffd3' />
-                        </HStack>
-                    </Flex>
-                    <Box fontSize='12px' textAlign='center'>
-                        Главная
-                    </Box>
+        <Flex
+            data-test-id='footer'
+            justify='space-around'
+            align='center'
+            className={styles.container}
+            display={{ base: 'flex', xl: 'none' }}
+        >
+            <Box>
+                <Flex justify='center' align='center' h='40px' className={styles.home}>
+                    <HStack>
+                        <HomeIcon color='#ffffd3' />
+                    </HStack>
+                </Flex>
+                <Box fontSize='12px' textAlign='center'>
+                    Главная
                 </Box>
+            </Box>
 
-                <Box>
-                    <Flex justify='center' align='center' h='40px'>
-                        <Icon as={SearchIcon} w='24px' h='24px' />
-                    </Flex>
-                    <Box fontSize='12px' textAlign='center'>
-                        Поиск
-                    </Box>
+            <Box>
+                <Flex justify='center' align='center' h='40px'>
+                    <Icon as={SearchIcon} w='24px' h='24px' />
+                </Flex>
+                <Box fontSize='12px' textAlign='center'>
+                    Поиск
                 </Box>
+            </Box>
 
-                <Box>
-                    <Flex justify='center' align='center' h='40px'>
-                        <Icon as={EditIcon} w='24px' h='24px' />
-                    </Flex>
-                    <Box fontSize='12px' textAlign='center'>
-                        Записать
-                    </Box>
+            <Box>
+                <Flex justify='center' align='center' h='40px'>
+                    <Icon as={EditIcon} w='24px' h='24px' />
+                </Flex>
+                <Box fontSize='12px' textAlign='center'>
+                    Записать
                 </Box>
+            </Box>
 
-                <Box>
-                    <Flex justify='center' align='center'>
-                        <Wrap className={styles.user}>
-                            <WrapItem>
-                                <Avatar
-                                    size='md'
-                                    bg='gray.500'
-                                    name='Екатерина Константинопольская'
-                                    src={avatar}
-                                />
-                            </WrapItem>
-                        </Wrap>
-                    </Flex>
-                    <Box fontSize='12px' textAlign='center'>
-                        Мой профиль
-                    </Box>
+            <Box>
+                <Flex justify='center' align='center'>
+                    <Wrap className={styles.user}>
+                        <WrapItem>
+                            <Avatar
+                                size='md'
+                                bg='gray.500'
+                                name='Екатерина Константинопольская'
+                                src={avatar}
+                            />
+                        </WrapItem>
+                    </Wrap>
+                </Flex>
+                <Box fontSize='12px' textAlign='center'>
+                    Мой профиль
                 </Box>
-            </Flex>
-        </footer>
+            </Box>
+        </Flex>
     );
 }
 
